02409c1ed36058dcba6e3609b858b2c7f8268d11
宮城 和音 <kazune.miyagi@bizreach.co.jp>
2018-10-03 15:18:04 +0900

    TDD Training on October, Thrid

--- scala/src/main/scala/tennis/TennisGame1.scala
+++ scala/src/main/scala/tennis/TennisGame1.scala
@@ -1,54 +1,65 @@
 package tennis
 
 
-class TennisGame1 (val player1Name : String, val player2Name : String) extends TennisGame {
-  var m_score1: Int = 0
-  var m_score2: Int = 0
+class TennisGame1(val player1Name: String, val player2Name: String) extends TennisGame {
+  var player1Score: Score = new Score()
+  var player2Score: Score = new Score()
 
-  def wonPoint(playerName : String) {
-          if (playerName == "player1")
-              m_score1 += 1
-          else
-              m_score2 += 1
-      }
+  def wonPoint(playerName: String) {
+    if (playerName == "player1")
+      player1Score.++
+    else
+      player2Score.++
+  }
 
-  def calculateScore() : String = {
-      var score : String = ""
-      var tempScore=0
-      if (m_score1==m_score2)
-      {
-        score = m_score1 match {
-              case 0 =>  "Love-All"
-              case 1 => "Fifteen-All"
-              case 2 => "Thirty-All"
-              case _ => "Deuce"
+  def calculateScore(): String = {
+    var score: String = ""
+    var tempScore = 0
+    if (player1Score.even(player2Score)) {
+      score = player1Score.point match {
+        case 0 => "Love-All"
+        case 1 => "Fifteen-All"
+        case 2 => "Thirty-All"
+        case _ => "Deuce"
+      }
+    }
+    else if (player1Score.isDeuce(player2Score)) {
+      val minusResult = player1Score.point - player2Score.point
+      if (minusResult == 1) score = "Advantage player1"
+      else if (minusResult == -1) score = "Advantage player2"
+      else if (minusResult >= 2) score = "Win for player1"
+      else score = "Win for player2"
+    }
+    else {
+      (1 to 2).foreach { i =>
+        if (i == 1) tempScore = player1Score.point
+        else {
+          score += "-"
+          tempScore = player2Score.point
+        }
+        val tempScore2 = tempScore match {
+          case 0 => "Love"
+          case 1 => "Fifteen"
+          case 2 => "Thirty"
+          case 3 => "Forty"
+        }
+        score += tempScore2
+      }
+    }
+    score
+  }
 
-          }
-      }
-      else if (m_score1>=4 || m_score2>=4)
-      {
-          val minusResult = m_score1-m_score2
-          if (minusResult==1) score ="Advantage player1"
-          else if (minusResult == -1) score ="Advantage player2"
-          else if (minusResult>=2) score = "Win for player1"
-          else score ="Win for player2"
-      }
-      else
-      {
-          for ( i<- 1 until 3 by 1)
-          {
-              if (i==1) tempScore = m_score1
-              else { score+="-"; tempScore = m_score2;}
-              val tempScore2 = tempScore match {
-                  case 0 => "Love"
-                  case 1 => "Fifteen"
-                  case 2 => "Thirty"
-                  case 3 => "Forty"
-              }
-            score += tempScore2
-          }
-      }
-    return score
+  class Score() {
+    def isDeuce(m_score2: Score): Boolean = point >= 4 || m_score2.point >= 4
+
+    def even(m_score2: Score): Boolean = point == m_score2.point
+
+    var point: Int = 0
+
+    def ++(): Score = {
+      point = point + 1
+      return this
+    }
   }
 
 }
